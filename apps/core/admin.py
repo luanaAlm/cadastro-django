7bb9e9eed30049d10205068e91be9d56170a245b
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-03-16 09:33:24 -0300

    Criação da app/alunos

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,6 +1,1 @@
 from django.contrib import admin
-from.models import Colaborador, Professor, Turma, Aluno
-
-
-
-admin.site.register(Aluno)
--- apps/core/models.py
+++ apps/core/models.py
@@ -1,15 +1,2 @@
 from django.db import models
 #https://www.youtube.com/watch?v=aFelj2GJPzw&t=53s
-
-
-
-
-
-    
-class Aluno(models.Model):
-    ID_Aluno = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=45)
-    turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.nome
