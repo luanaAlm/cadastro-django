b4ad52ad9f5c8bdd910345a2420b4338254e75fa
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-03-16 15:41:14 -0300

    Criação do form aluno

--- apps/alunos/urls.py
+++ apps/alunos/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import homeAlunos, listarAlunos
+from .views import homeAlunos, listarAlunos, criarAluno, alunoNovo
 
 
 #urls Alunos
 urlpatterns = [
     path('',homeAlunos),
     path('listar_alunos',listarAlunos, name='listar_alunos'),
+    path('criar_alunos',criarAluno, name='criar_alunos'),
+    path('novo_aluno',alunoNovo, name='novo_aluno'),
 ]
 
--- apps/alunos/views.py
+++ apps/alunos/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Aluno
- 
+from .form import AlunoForm
+
 #views Alunos
 @login_required
 def homeAlunos(request):
@@ -12,3 +13,15 @@ def homeAlunos(request):
 def listarAlunos(request):
     alunos = Aluno.objects.all()
     return render(request, 'listar_alunos.html', {'alunos': alunos})
+
+@login_required
+def criarAluno(request):
+    form = AlunoForm()
+    return render(request, 'criar_alunos.html', {'form': form})
+
+@login_required
+def alunoNovo(request):
+    form = AlunoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('listar_alunos')
