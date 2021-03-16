f1b2d35125750f998ca2ad683e18603e080485e8
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-03-16 08:59:46 -0300

    Criação da app/turmas

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from.models import Colaborador, Professor, Turma, Aluno
 
 
-admin.site.register(Turma)
+
 admin.site.register(Aluno)
--- apps/core/models.py
+++ apps/core/models.py
@@ -4,13 +4,6 @@ from django.db import models
 
 
 
-class Turma(models.Model):
-    ID_Turma = models.AutoField(primary_key=True)
-    turma = models.CharField(max_length=45)
-    Professor = models.ManyToManyField(Professor)
-
-    def __str__(self):
-        return self.turma
 
     
 class Aluno(models.Model):
