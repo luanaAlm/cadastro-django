b04ac41189f0894cc3c400b6017a93457be960f1
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-03-16 08:51:47 -0300

    Criação da app

---
+++ apps/core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from.models import Colaborador, Professor, Turma, Aluno
+
+
+admin.site.register(Colaborador)
+admin.site.register(Professor)
+admin.site.register(Turma)
+admin.site.register(Aluno)
---
+++ apps/core/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+#https://www.youtube.com/watch?v=aFelj2GJPzw&t=53s
+
+class Colaborador(models.Model):
+    ID_colaborador = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=45)
+    data = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Professor(models.Model):
+    ID_Professor = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=45)
+    data = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Turma(models.Model):
+    ID_Turma = models.AutoField(primary_key=True)
+    turma = models.CharField(max_length=45)
+    Professor = models.ManyToManyField(Professor)
+
+    def __str__(self):
+        return self.turma
+
+    
+class Aluno(models.Model):
+    ID_Aluno = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=45)
+    turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
