2ddfd0a8b09c4e53729f0097e9a62ad8e0da9674
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-03-17 18:56:17 -0300

    Alterações Alunos e Colaboradores

--- apps/colaboradores/models.py
+++ apps/colaboradores/models.py
@@ -6,7 +6,6 @@ class Colaborador(models.Model):
     ID_colaborador = models.AutoField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     nome = models.CharField(max_length=45)
-    data = models.DateField()
-
+    
     def __str__(self):
         return self.nome
--- ebdcadastro/urls.py
+++ ebdcadastro/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('turmas/', include('apps.turmas.urls')),
     path('alunos/', include('apps.alunos.urls')),
     path('professores/', include('apps.professores.urls')),
-    path('colaboradores/', include('apps.colaboradores.urls')),
+    #path('colaboradores/', include('apps.colaboradores.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
