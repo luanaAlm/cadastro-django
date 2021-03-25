51c4bd29d83cbab46ac709db8c10cdf2a38f1bf5
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-03-24 22:45:35 -0300

    turmas

---
+++ apps/turmas/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Turma(models.Model):
+    ID_Turma = models.AutoField(primary_key=True)
+    turma = models.CharField(max_length=45)
+    MODALIDADE_CHOICES = (
+        ('Adolescentes', 'Adolescentes'),
+        ('Adulto', 'Adulto'),
+        ('Infantil', 'Infantil'),
+        ('Jovens', 'Jovens'),
+        ('Juvenis','Juvenis'),
+        ('Pré-Adolescentes', 'Pré-Adolescentes'),
+    )
+    modalidade = models.CharField(max_length=100, choices=MODALIDADE_CHOICES)
+    
+    
+    def __str__(self):
+        return self.turma
--- ebdcadastro/settings.py
+++ ebdcadastro/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.funcionarios',
     'apps.alunos',
+    'apps.turmas',
     #'apps.professores',
     #'apps.colaboradores',
-    #'apps.turmas',
     #'apps.coperadores',
 ]
 
