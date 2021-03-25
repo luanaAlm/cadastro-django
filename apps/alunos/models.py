d97eb88f7dc8e838ea01a381c73bf8c3a2b6628b
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-03-24 22:50:16 -0300

    alunos

--- apps/alunos/models.py
+++ apps/alunos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from  apps.turmas.models import Turma
 
-# Create your models here.
+
+class Aluno(models.Model):
+    SEXO_CHOICES = (
+        ('Masculino', 'Masculino'),
+        ('Feminino', 'Feminino'),
+    )
+    ID_Aluno = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=45)
+    turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
+    sexo = models.CharField(max_length=100, choices=SEXO_CHOICES)
+    data = models.DateField()
+
+    def __str__(self):
+        return self.nome
