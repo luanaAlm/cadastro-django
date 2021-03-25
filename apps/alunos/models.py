9c9051c913b08a08c7c000d575d114c163dc86d6
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-03-24 22:02:20 -0300

    Revert "Adição de novos campos em Alunos"
    This reverts commit 94c01178dfe7ab65eb226b55663ae281738b97e9.

--- apps/alunos/models.py
+++ apps/alunos/models.py
@@ -3,15 +3,9 @@ from  apps.turmas.models import Turma
 
 
 class Aluno(models.Model):
-    SEXO_CHOICES = (
-        ('Masculino', 'Masculino'),
-        ('Feminino', 'Feminino'),
-    )
     ID_Aluno = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=45)
     turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
-    sexo = models.CharField(max_length=100, choices=SEXO_CHOICES)
-    data = models.DateField()
 
     def __str__(self):
         return self.nome
