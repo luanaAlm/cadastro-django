cbc7749015ed8051039559faff190cd2a6a33b39
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-03-24 23:54:40 -0300

    Relacionamento cpf

--- apps/validacpfs/models.py
+++ apps/validacpfs/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from cpf_field.models import CPFField
+from apps.alunos.models import Aluno
+from apps.professores.models import Professor
 
 class ValidaCpf(models.Model):
     cpf = CPFField()
+    aluno = models.OneToOneField(Aluno, on_delete=models.SET_NULL, null=True)
+    Professor = models.OneToOneField(Professor, on_delete=models.SET_NULL, null=True)
+
 
     def __str__(self):
         return self.cpf
