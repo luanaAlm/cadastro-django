1e540d3173faf553f4f5f8d13d38a67950c180d3
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-04-03 10:11:51 -0300

    visualizar_alunos

--- ebdcadastro/urls.py
+++ ebdcadastro/urls.py
@@ -27,7 +27,4 @@ urlpatterns = [
     path('funcionarios/', include('apps.funcionarios.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-]
-
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
