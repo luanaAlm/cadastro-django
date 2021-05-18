1811114e4325d00954b222cbec13f71ddcff1bdd
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-05-18 15:58:46 -0300

    Alterações do pdf em visualizar_professor.html

--- static/js/pdf.js
+++ static/js/pdf.js
@@ -6,7 +6,7 @@ window.onload = function(){
         console.log(window)
         var opt = {
             margin: 1,
-            filename: 'alunos.pdf',
+            filename: 'ebd.pdf',
             image: { type: 'jpeg', quality: 0.98 },
             html2canvas: { scale: 2 },
             jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
