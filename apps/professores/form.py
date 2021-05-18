f67ddbe10fe5d735897199714a0ef297d8043750
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-05-18 15:50:33 -0300

    Inserção dos campo model professor

--- apps/professores/form.py
+++ apps/professores/form.py
@@ -1,7 +1,13 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Professor
+from django.forms.widgets import ClearableFileInput
+
 
 class ProfessorForm(ModelForm):
+
+    imagem = forms.ImageField(widget=ClearableFileInput)
+
     class Meta:
         model = Professor
         fields = '__all__'
--- apps/professores/models.py
+++ apps/professores/models.py
@@ -1,12 +1,62 @@
 from django.db import models
 from apps.turmas.models import Turma
+from cpf_field.models import CPFField
 
 
 class Professor(models.Model):
+    SEXO_CHOICES = (
+        ('Masculino', 'Masculino'),
+        ('Feminino', 'Feminino'),
+    )
+    STATE_CHOICES = (
+        ('AC', 'Acre'),
+        ('AL', 'Alagoas'),
+        ('AP', 'Amapá'),
+        ('AM', 'Amazonas'),
+        ('BA', 'Bahia'),
+        ('CE', 'Ceará'),
+        ('DF', 'Distrito Federal'),
+        ('ES', 'Espírito Santo'),
+        ('GO', 'Goiás'),
+        ('MA', ' Maranhão '),
+        (' MT ', ' Mato Grosso '),
+        (' MS ', ' Mato Grosso do Sul '),
+        (' MG ', ' Minas Gerais '),
+        (' PA ', ' Pará '),
+        ('PB', 'Paraíba'),
+        ('PR', 'Paraná'),
+        ('PE', 'Pernambuco'),
+        ('PI', 'Piauí'),
+        ('RJ', 'Rio de Janeiro'),
+        ('RN', 'Rio Grande do Norte'),
+        ('RS', 'Rio Grande do Sul'),
+        ('RO', 'Rondônia'),
+        ('RR', 'Roraima '),
+        (' SC ', ' Santa Catarina '),
+        (' SP ', ' São Paulo '),
+        (' SE ', ' Sergipe '),
+        (' TO ', ' Tocantins ')
+    )
+
     ID_Professor = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=45)
-    data = models.DateField()
     turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
+
+    nome = models.CharField(max_length=45)
+    sexo = models.CharField(max_length=100, choices=SEXO_CHOICES)
+    data = models.DateField(max_length=8)
+    cpf = CPFField(max_length=14)
+    telefone = models.IntegerField(blank=False, null=False, max_length=16)
+    email = models.EmailField(max_length=200, null=True)
+    # endereço
+    cep = models.IntegerField(max_length=8)
+    endereco = models.CharField(max_length=100, blank=False, null=False)
+    numero = models.IntegerField(null=True, blank=True)
+    complemento = models.CharField(max_length=1000, null=True)
+    bairro = models.CharField(max_length=100, blank=False, null=False)
+    municipio = models.CharField(max_length=100, blank=False, null=False)
+    estado = models.CharField(max_length=100, choices=STATE_CHOICES)
+    # imagem
+    imagem = models.ImageField(upload_to="img/%y")
 
     def __str__(self):
         return self.nome
