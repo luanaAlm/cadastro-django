3ea90e18245e10a7ccd30bb29d22c52ae59eb296
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-05-22 16:14:51 -0300

    Alterações em turma

--- apps/turmas/views.py
+++ apps/turmas/views.py
@@ -9,12 +9,13 @@ from apps.professores.models import Professor
 
 @login_required
 def homeTurmas(request):
-    turmas = Turma.objects.all()
+    turmas = Turma.objects.all().order_by("modalidade")
     return render(request, 'home_turmas.html', {'turmas': turmas})
 
 
 @login_required
 def listarTurmas(request, turma_id):
-    alunos = Aluno.objects.filter(turma_id=turma_id)
-    professores = Professor.objects.filter(turma_id=turma_id)
-    return render(request, 'listar_turmas.html', {'alunos': alunos, 'professores': professores})
+    titulos = Turma.objects.filter(ID_Turma=turma_id)
+    alunos = Aluno.objects.filter(turma_id=turma_id).order_by("nome")
+    professores = Professor.objects.filter(turma_id=turma_id).order_by("nome")
+    return render(request, 'listar_turmas.html', {'titulos': titulos, 'alunos': alunos, 'professores': professores})
