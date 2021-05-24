e08e9c96e3c66d7a15d29a07c91d43ab70e8d973
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-05-24 10:57:13 -0300

    Criação da lista de colaboradores

--- apps/administracao/urls.py
+++ apps/administracao/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import criarColaborador, colaboradorNovo
+from .views import criarColaborador, colaboradorNovo, listarColaboradores, consulta
 
 # urls Professores
 urlpatterns = [
     path('criar_colaborador', criarColaborador, name='criar_colaborador'),
     path('novo_colaborador', colaboradorNovo, name='novo_colaborador'),
+    path('listar_colaboradores', listarColaboradores,
+         name='listar_colaboradores'),
+    path('consulta/', consulta),
 
 ]
--- apps/administracao/views.py
+++ apps/administracao/views.py
@@ -3,6 +3,34 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Administracao
 from .form import ColaboradorForm
+from django.views.decorators.csrf import csrf_protect
+
+
+@login_required
+def listarColaboradores(request):
+    titulo = 'Colaborador'
+    subtitle = 'Lista de Colaboradores'
+    termo_busca = request.GET.get('pesquisa', None)
+    if termo_busca:
+        colaboradores = Administracao.objects.all()
+        colaboradores = Administracao.filter(nome__contains=termo_busca)
+    else:
+        colaboradores = Administracao.objects.all()
+    return render(request, 'listar_colaboradores.html', {'titulo': titulo, 'subtitle': subtitle, 'colaboradores': colaboradores})
+
+
+@login_required
+@csrf_protect
+def consulta(request):
+    consulta = request.POST.get('consulta')
+    campo = request.POST.get('campo')
+
+    if campo == 'nome':
+        colaboradores = Administracao.objects.filter(nome__contains=consulta)
+    elif campo == 'funcao':
+        colaboradores = Administracao.objects.filter(funcao__contains=consulta)
+
+    return render(request, 'listar_colaboradores.html', {'colaboradores': colaboradores})
 
 
 @login_required
@@ -18,4 +46,4 @@ def colaboradorNovo(request):
     form = ColaboradorForm(request.POST, request.FILES)
     if form.is_valid():
         form.save()
-    return redirect('home')
+    return redirect('listar_colaboradores')
